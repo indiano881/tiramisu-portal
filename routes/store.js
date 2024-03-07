b82d08e9bdbfc5e0f0dfdfa08389730abd85c3c2
Davide Baldi <d.baldi88@gmail.com>
2024-03-07 15:34:59 +0100

    improvment of sub-routing-NEED TO CHECK make order vbutton

--- routes/store.js
+++ routes/store.js
@@ -41,4 +41,30 @@ storeRouter.get("/catalog",(req,res)=>{
     })
 })
 
+storeRouter.get("/tir", (req, res) => {
+    const index = req.query.index;
+    const selectedTiramisu = tiramisuArray[index];
+    
+    
+    res.render("pages/single-tiramisu.ejs", 
+     {
+        page: "tiramisu",
+       selectedTiramisu, 
+       tiramisuArray,
+       mainMessage: selectedTiramisu.name,
+       dayOfTheWeek: day,
+       name: selectedTiramisu.name,
+       description: selectedTiramisu.description,
+       image: "../"+selectedTiramisu.image,
+       price: selectedTiramisu.price,
+       calories: selectedTiramisu.calories,
+       ing1: selectedTiramisu.ingredient1,
+       ing2: selectedTiramisu.ingredient2,
+       ing3: selectedTiramisu.ingredient3,
+       ing4: selectedTiramisu.ingredient4,
+       ing5: selectedTiramisu.ingredient5,
+       ing6: selectedTiramisu.ingredient6
+     });
+ });
+
 export default storeRouter;
