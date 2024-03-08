92877615d8aaf7489a5345d8f28b715bb8fe4495
Davide Baldi <d.baldi88@gmail.com>
2024-03-08 12:14:06 +0100

    project finished need to clean up

--- routes/history.js
+++ routes/history.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { day } from "../data/tiramisu.js";
+import { day, tiramisuArray } from "../data/tiramisu.js";
 
 
 
@@ -41,5 +41,30 @@ historyRouter.get("/mascarpone",(req,res)=>{
         
     })
 })
+historyRouter.get("/tir", (req, res) => {
+    const index = req.query.index;
+    const selectedTiramisu = tiramisuArray[index];
+    
+    
+    res.render("pages/single-tiramisu.ejs", 
+     {
+        page: "tiramisu",
+       selectedTiramisu, 
+       tiramisuArray,
+       mainMessage: selectedTiramisu.name,
+       dayOfTheWeek: day,
+       name: selectedTiramisu.name,
+       description: selectedTiramisu.description,
+       image: "../"+selectedTiramisu.image,
+       price: selectedTiramisu.price,
+       calories: selectedTiramisu.calories,
+       ing1: selectedTiramisu.ingredient1,
+       ing2: selectedTiramisu.ingredient2,
+       ing3: selectedTiramisu.ingredient3,
+       ing4: selectedTiramisu.ingredient4,
+       ing5: selectedTiramisu.ingredient5,
+       ing6: selectedTiramisu.ingredient6
+     });
+ });
 
 export default historyRouter;
