cf5ad6f283afc8c3167ad8d0d7a16fb43dacd1e7
Davide Baldi <d.baldi88@gmail.com>
2024-03-08 09:47:24 +0100

    add a con link

--- routes/history.js
+++ routes/history.js
@@ -14,5 +14,32 @@ historyRouter.get("/",(req,res)=>{
         
     })
 })
+historyRouter.get("/treviso",(req,res)=>{
+    res.render("pages/home.ejs",
+    {
+        page: "treviso",
+        mainMessage: "About Treviso",
+        dayOfTheWeek: day
+        
+    })
+})
+historyRouter.get("/savoiardi",(req,res)=>{
+    res.render("pages/home.ejs",
+    {
+        page: "savoiardi",
+        mainMessage: "Savoiardi",
+        dayOfTheWeek: day
+        
+    })
+})
+historyRouter.get("/mascarpone",(req,res)=>{
+    res.render("pages/home.ejs",
+    {
+        page: "mascarpone",
+        mainMessage: "Mascarpone",
+        dayOfTheWeek: day
+        
+    })
+})
 
 export default historyRouter;
