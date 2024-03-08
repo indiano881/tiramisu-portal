20b04641372e35d729dad1d04d0564c05a43c8c2
Davide Baldi <d.baldi88@gmail.com>
2024-03-08 09:55:12 +0100

    subrouting 2 almost done-NEED TO CONTINUE WITH MAIN CONTENT

--- routes/history.js
+++ routes/history.js
@@ -6,7 +6,7 @@ import { day } from "../data/tiramisu.js";
 const historyRouter=express.Router();
 
 historyRouter.get("/",(req,res)=>{
-    res.render("pages/home.ejs",
+    res.render("pages/history.ejs",
     {
         page: "history",
         mainMessage: "History of Tiramisu",
@@ -15,27 +15,27 @@ historyRouter.get("/",(req,res)=>{
     })
 })
 historyRouter.get("/treviso",(req,res)=>{
-    res.render("pages/home.ejs",
+    res.render("pages/history.ejs",
     {
-        page: "treviso",
+        page: "history/treviso",
         mainMessage: "About Treviso",
         dayOfTheWeek: day
         
     })
 })
 historyRouter.get("/savoiardi",(req,res)=>{
-    res.render("pages/home.ejs",
+    res.render("pages/history.ejs",
     {
-        page: "savoiardi",
+        page: "history/savoiardi",
         mainMessage: "Savoiardi",
         dayOfTheWeek: day
         
     })
 })
 historyRouter.get("/mascarpone",(req,res)=>{
-    res.render("pages/home.ejs",
+    res.render("pages/history.ejs",
     {
-        page: "mascarpone",
+        page: "history/mascarpone",
         mainMessage: "Mascarpone",
         dayOfTheWeek: day
         
