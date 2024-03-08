6eaf60563b3045ddebde083558ba45393557e667
Davide Baldi <d.baldi88@gmail.com>
2024-03-08 13:03:27 +0100

    cleand completed- nedd top do CSS only

--- public/js/function.js
+++ public/js/function.js
@@ -3,9 +3,4 @@ $(()=> {
     $(".history").hide().fadeIn(3000);
     $(".company").hide().fadeIn(3000);
     $(".location").hide().fadeIn(3000);
-
-    
-    
-      
-
 })
--- public/js/navigation.js
+++ public/js/navigation.js
@@ -1,8 +1,7 @@
 $(()=> {
-        // GENERAL TO ALL PAGES
-        //Hamburger menu 1st level/wokrks
-      
+
         const navMenu = () => {
+
           $("nav .hamburger-menu").on("click", (event) => $(event.target).next("ul").toggle());
           $("ul li").on({
             mouseenter: (event) => $(event.target).toggleClass("active"),
@@ -11,22 +10,17 @@ $(()=> {
               $(event.target).find(".submenu").toggle("active");
               $(event.target).siblings().find(".submenu").hide("active");
             },
-          });
-          //hamburger 2nd level-works
+          })
+          
           $(".submenu li").on("click", (event) => $(event.target).next("ul").toggle());
           $(".submenu li").on({
             click: (event) => {
               $(event.target).find(".submenu-2").toggle("active");
               $(event.target).siblings().find(".submenu-2").hide("active");
             },
-          });
-          
-          
-        };
+          })
+        
+        }
       
         navMenu();
-        
-     
-      
-
 })
